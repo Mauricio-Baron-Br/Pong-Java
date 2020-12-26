43cbda9db7687066320effffeb4ae1ec92c398f6
Mauricio Baron <mauriciobaron2014@gmail.com>
2020-12-26 15:21:52 -0300

    Construtor da Bolinha, pintando o fundo e bolinha

--- src/boardgame/Board.java
+++ src/boardgame/Board.java
@@ -1,22 +1,41 @@
 package boardgame;
 
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.geom.Rectangle2D;
+
 import javax.swing.JFrame;
+import javax.swing.JPanel;
 
 public class Board extends JFrame {
-	
-	
-	
+
+	Grafico gr;
+
+	class Grafico extends JPanel {
+		@Override
+		public void paintComponent(Graphics g) {
+			super.paintComponent(g);
+			setBackground(Color.BLACK);
+
+			Graphics2D boll = (Graphics2D) g;
+			boll.setColor(Color.WHITE);
+			boll.fill(new Rectangle2D.Double(20, 20, 20, 20));
+
+		}
+
+	}
+
 	public void Janela() {
 		setTitle("Pong-Java");
-		setSize(800,600);
+		setSize(800, 600);
 		setLocationRelativeTo(null);
 		setResizable(false);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setVisible(true);
-		
-		
+		gr = new Grafico();
+		add(gr);
+
 	}
-	
-	
 
 }
