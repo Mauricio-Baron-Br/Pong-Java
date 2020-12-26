32c8127613e4131547c0986a6ccc419033906948
Mauricio Baron <mauriciobaron2014@gmail.com>
2020-12-26 15:35:33 -0300

    Criaçao do tabuleiro e jogadores

--- src/boardgame/Board.java
+++ src/boardgame/Board.java
@@ -18,10 +18,30 @@ public class Board extends JFrame {
 			super.paintComponent(g);
 			setBackground(Color.BLACK);
 
+			//bar0
+			Graphics2D bar0 = (Graphics2D) g;
+			bar0.setColor(Color.WHITE);
+			bar0.fill(new Rectangle2D.Double(40, 20, 700, 20));
+			//bar1
+			Graphics2D bar1 = (Graphics2D) g;
+			bar1.setColor(Color.WHITE);
+			bar1.fill(new Rectangle2D.Double(40, 520, 700, 20));
+			//bar2
+			Graphics2D bar2 = (Graphics2D) g;
+			bar2.setColor(Color.WHITE);
+			bar2.fill(new Rectangle2D.Double(400, 20, 5, 500));
+			//play0
+			Graphics2D play0 = (Graphics2D) g;
+			play0.setColor(Color.WHITE);
+			play0.fill(new Rectangle2D.Double(40, 20, 20, 100));
+			//play1
+			Graphics2D play1 = (Graphics2D) g;
+			play1.setColor(Color.WHITE);
+			play1.fill(new Rectangle2D.Double(720, 20, 20, 100));
+			//bolinha
 			Graphics2D boll = (Graphics2D) g;
 			boll.setColor(Color.WHITE);
-			boll.fill(new Rectangle2D.Double(20, 20, 20, 20));
-
+			boll.fill(new Rectangle2D.Double(20, 20, 15, 15));
 		}
 
 	}
