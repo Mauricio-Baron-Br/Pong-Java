7bfb8e10cd2da7febc3e698364601c3118a99c1e
Mauricio Baron <mauriciobaron2014@gmail.com>
2020-12-26 16:21:37 -0300

    criacao do movimento bolinha,jogador(control,jogado)

--- src/application/Pong.java
+++ src/application/Pong.java
@@ -7,6 +7,7 @@ public class Pong {
 	public static void main(String[] args) {
 		Board board = new Board();
 		board.Janela();
+		board.control();
 
 	}
 
--- src/boardgame/Board.java
+++ src/boardgame/Board.java
@@ -3,16 +3,23 @@ package boardgame;
 import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 import java.awt.geom.Rectangle2D;
+
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
+import javax.swing.Timer;
 
-public class Board extends JFrame {
+public class Board extends JFrame implements ActionListener {
 
 	private Grafico gr;
-	private int bx = 50, by = 50, velx=2, vely = 3;
-	private int px = 10, py = 100, iax = 750, iay = 100;  
+	private Timer time;
+	private int bx = 50, by = 50, velx = 4, vely = 3;
+	private int px = 10, py = 100, iax = 750, iay = 100;
 
 	class Grafico extends JPanel {
 		@Override
@@ -20,27 +27,27 @@ public class Board extends JFrame {
 			super.paintComponent(g);
 			setBackground(Color.BLACK);
 
-			//bar0
+			// bar0
 			Graphics2D bar0 = (Graphics2D) g;
 			bar0.setColor(Color.WHITE);
 			bar0.fill(new Rectangle2D.Double(40, 20, 700, 20));
-			//bar1
+			// bar1
 			Graphics2D bar1 = (Graphics2D) g;
 			bar1.setColor(Color.WHITE);
 			bar1.fill(new Rectangle2D.Double(40, 520, 700, 20));
-			//bar2
+			// bar2
 			Graphics2D bar2 = (Graphics2D) g;
 			bar2.setColor(Color.WHITE);
 			bar2.fill(new Rectangle2D.Double(400, 20, 5, 500));
-			//play0
+			// play0
 			Graphics2D play0 = (Graphics2D) g;
 			play0.setColor(Color.WHITE);
 			play0.fill(new Rectangle2D.Double(40, py, 20, 100));
-			//play1
+			// play1
 			Graphics2D play1 = (Graphics2D) g;
 			play1.setColor(Color.WHITE);
 			play1.fill(new Rectangle2D.Double(720, iay, 20, 100));
-			//bolinha
+			// bolinha
 			Graphics2D boll = (Graphics2D) g;
 			boll.setColor(Color.WHITE);
 			boll.fill(new Rectangle2D.Double(bx, by, 15, 15));
@@ -48,6 +55,58 @@ public class Board extends JFrame {
 
 	}
 
+	@Override
+	public void actionPerformed(ActionEvent arg0) {
+		Jogar();
+	}
+
+	private void Jogar() {
+		bx += velx;
+		by += vely;
+		
+		if(bx > 750) {
+			velx *= -1;
+		}
+		if(bx < 60 && bx > 50 && by > py &&by < py + 100) {
+			velx *= -1;
+		}
+		if(by > 500 || by <=40) {
+			vely *= -1;
+		}
+		if(bx < -40) {
+			bx = 300;
+			velx = 3; //velocidade bolinha
+		}
+		gr.repaint();
+		
+	}
+
+	public void control() {
+		addKeyListener(new KeyListener() {
+
+			@Override
+			public void keyTyped(KeyEvent arg0) {
+
+			}
+
+			@Override
+			public void keyReleased(KeyEvent arg0) {
+				if (arg0.getKeyCode() == 38 && py > 40) {
+					//altera velocidade do jogador
+					py -= 40;
+				} else if (arg0.getKeyCode() == 40 && py < 420) {
+					py += 40;
+				}
+
+			}
+
+			@Override
+			public void keyPressed(KeyEvent arg0) {
+
+			}
+		});
+	}
+	
 	public void Janela() {
 		setTitle("Pong-Java");
 		setSize(800, 600);
@@ -57,7 +116,10 @@ public class Board extends JFrame {
 		setVisible(true);
 		gr = new Grafico();
 		add(gr);
+		time = new Timer(2, this);
+		time.start();
 
+		
 	}
 
 }
