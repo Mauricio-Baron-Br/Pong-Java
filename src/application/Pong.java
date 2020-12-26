cbcaa5056c5701a94645a68dada26d3590008f64
Mauricio Baron <mauriciobaron2014@gmail.com>
2020-12-26 15:15:42 -0300

    Construtor da Janela criado

--- src/application/Pong.java
+++ src/application/Pong.java
@@ -5,8 +5,9 @@ import boardgame.Board;
 public class Pong {
 
 	public static void main(String[] args) {
-	
-	
+		Board board = new Board();
+		board.Janela();
+
 	}
 
 }
--- src/boardgame/Board.java
+++ src/boardgame/Board.java
@@ -5,5 +5,18 @@ import javax.swing.JFrame;
 public class Board extends JFrame {
 	
 	
+	
+	public void Janela() {
+		setTitle("Pong-Java");
+		setSize(800,600);
+		setLocationRelativeTo(null);
+		setResizable(false);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setVisible(true);
+		
+		
+	}
+	
+	
 
 }
