932a7b18c859dff4f0e2152647e34582a9bc3e9b
Mauricio Baron <mauriciobaron2014@gmail.com>
2020-12-26 15:11:01 -0300

    Projeto Criado

---
+++ src/application/Pong.java
@@ -0,0 +1,12 @@
+package application;
+
+import boardgame.Board;
+
+public class Pong {
+
+	public static void main(String[] args) {
+	
+	
+	}
+
+}
---
+++ src/boardgame/Board.java
@@ -0,0 +1,9 @@
+package boardgame;
+
+import javax.swing.JFrame;
+
+public class Board extends JFrame {
+	
+	
+
+}
