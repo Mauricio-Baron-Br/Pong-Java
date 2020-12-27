6bc69b5610fd5c07279ef531db515f4133e2fd89
Mauricio Baron <mauriciobaron2014@gmail.com>
2020-12-27 10:46:06 -0300

    alteracao de pasta de som e fonte, melhoria no cogigo

--- src/boardgame/Board.java
+++ src/boardgame/Board.java
@@ -7,7 +7,6 @@ import java.awt.Font;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.Paint;
-import java.awt.desktop.ScreenSleepEvent;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
@@ -20,26 +19,27 @@ import javax.swing.Timer;
 
 public class Board extends JFrame implements ActionListener {
 
-	private URL UD = Paint.class.getResource("\\somfont\\upDow.wav");
-	private URL Pong = Paint.class.getResource("\\somfont\\ppong.wav");
-	private URL Fora = Paint.class.getResource("\\somfont\\fora.wav");
+	private URL UD = Paint.class.getResource("util\\upDow.wav");
+	private URL Pong = Paint.class.getResource("util\\ppong.wav");
+	private URL Fora = Paint.class.getResource("util\\fora.wav");
+
 	private Font big = new Font("Arial", Font.BOLD, 70);
+	private Font vidas = new Font("Arial", Font.ITALIC, 0);
 	private Grafico gr;
 	private Timer time;
-	private int bx = 50, by = 50, velx = 4, vely = 3, dir = 1, vida = 1;
+	private int bx = 50, by = 50, velx = 3, vely = 3, dir = 1, vida = 5;
 	private int px = 10, py = 100, iax = 750, iay = 100, play0Score = 0, play1Score = 0;
 
 	AudioClip ud = Applet.newAudioClip(UD);
 	AudioClip pong = Applet.newAudioClip(Pong);
 	AudioClip fora = Applet.newAudioClip(Fora);
-	
+
 	class Grafico extends JPanel {
 		@Override
 		public void paintComponent(Graphics g) {
 			super.paintComponent(g);
 			setBackground(Color.BLACK);
 
-			// bar0
 			Graphics2D bar0 = (Graphics2D) g;
 			Graphics2D bar1 = (Graphics2D) g;
 			Graphics2D bar2 = (Graphics2D) g;
@@ -48,41 +48,53 @@ public class Board extends JFrame implements ActionListener {
 			Graphics2D boll = (Graphics2D) g;
 			Graphics2D scor = (Graphics2D) g;
 			scor.setFont(big);
-			if(vida > 0){
-			bar0.setColor(Color.WHITE);
-			bar1.setColor(Color.WHITE);
-			bar2.setColor(Color.WHITE);
-			play0.setColor(Color.WHITE);
-			play1.setColor(Color.WHITE);
-			boll.setColor(Color.WHITE);
-			}else {
-				py = by - 50;
-				scor.drawString("PONG  JAVA",180, 220);
-			}
 			
+			if (vida > 0) {
+				bar0.setColor(Color.WHITE);
+				bar1.setColor(Color.WHITE);
+				bar2.setColor(Color.WHITE);
+				play0.setColor(Color.WHITE);
+				play1.setColor(Color.WHITE);
+				boll.setColor(Color.WHITE);
+				scor.setColor(Color.WHITE);
+			} else {
+				py = by - 50;
+				scor.drawString("PONG  JAVA", 180, 220);
+			}
+
 			bar0.fill(new Rectangle2D.Double(40, 20, 700, 20));
 			bar1.fill(new Rectangle2D.Double(40, 520, 700, 20));
 			bar2.fill(new Rectangle2D.Double(400, 20, 5, 500));
 			play0.fill(new Rectangle2D.Double(40, py, 20, 100));
 			play1.fill(new Rectangle2D.Double(720, iay, 20, 100));
 			boll.fill(new Rectangle2D.Double(bx, by, 15, 15));
-			
-			
-			
-			scor.setColor(Color.WHITE);
 			scor.drawString(play0Score + "      " + play1Score, 310, 100);
 
 		}
 
 	}
 
+	public void Janela() {
+		setTitle("Pong-Java");
+		setSize(800, 600);
+		setLocationRelativeTo(null);
+		setResizable(false);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setVisible(true);
+		gr = new Grafico();
+		add(gr);
+		time = new Timer(2, this);
+		time.start();
+
+	}
+
 	@Override
 	public void actionPerformed(ActionEvent arg0) {
-		Jogar();
+		jogar();
 	}
 
 	@SuppressWarnings("deprecation")
-	private void Jogar() {
+	private void jogar() {
 		bx += velx;
 		by += vely;
 
@@ -91,7 +103,7 @@ public class Board extends JFrame implements ActionListener {
 			play1Score++;
 			pong.play();
 		}
-		if (bx < 60 && bx > 50 && by + 14 > py && by < py + 100) {
+		if (bx < 60 && bx > 50 && by > py && by < py + 100) {
 			velx *= -1;
 			play0Score++;
 			pong.play();
@@ -105,9 +117,10 @@ public class Board extends JFrame implements ActionListener {
 			velx = 3; // velocidade bolinha
 			fora.play();
 			vida--;
+
 		}
 		if (bx > 400) {
-			if (by > iay + 50 && iay < 400) {
+			if (by > iay + 50 && iay < 420) {
 				iay += 3;
 			}
 			if (by < iay && iay > 40) {
@@ -117,6 +130,7 @@ public class Board extends JFrame implements ActionListener {
 		}
 
 		gr.repaint();
+		
 		// melhora no movimento do jogador
 		if (dir == 1 && py > 40) {
 			py -= 3;
@@ -136,36 +150,20 @@ public class Board extends JFrame implements ActionListener {
 
 			@Override
 			public void keyReleased(KeyEvent arg0) {
+				dir = 0;
+			}
 
+			@Override
+			public void keyPressed(KeyEvent arg0) {
+				
 				if (arg0.getKeyCode() == 38 && py > 40) {
 					// altera velocidade do jogador
 					dir = 1;
 				} else if (arg0.getKeyCode() == 40 && py < 420) {
 					dir = 2;
 				}
-
-			}
-
-			@Override
-			public void keyPressed(KeyEvent arg0) {
-				dir = 0;
-
 			}
 		});
-	}
-
-	public void Janela() {
-		setTitle("Pong-Java");
-		setSize(800, 600);
-		setLocationRelativeTo(null);
-		setResizable(false);
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setVisible(true);
-		gr = new Grafico();
-		add(gr);
-		time = new Timer(2, this);
-		time.start();
-
 	}
 
 }
